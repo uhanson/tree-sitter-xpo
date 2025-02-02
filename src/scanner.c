21cfbaadc0d397fbcfc1fef6be583d8c8f6413c7
uhanson <andrey.anikin@gmail.com>
2025-02-02 20:37:26 +0300

    Simple counting indent without tracking it in stack

--- src/scanner.c
+++ src/scanner.c
@@ -1,6 +1,14 @@
 #include "tree_sitter/array.h"
 #include "tree_sitter/parser.h"
 
+// #define DEBUG
+
+#ifdef DEBUG
+#include <ctype.h>
+#endif
+
+#define INDENT_SIZE 2
+
 enum TokenType {
     INDENT,
     DEDENT,
@@ -11,7 +19,10 @@ enum TokenType {
 };
 
 typedef struct {
-    Array(uint32_t) indents;
+    uint32_t last_indent;
+#ifdef DEBUG
+    uint32_t row;
+#endif
 } Scanner;
 
 static inline void advance(TSLexer *lexer) { lexer->advance(lexer, false); }
@@ -22,11 +33,9 @@ bool tree_sitter_xpo_external_scanner_scan(void *payload, TSLexer *lexer, const 
     Scanner *scanner = (Scanner *)payload;
 
     lexer->mark_end(lexer);
-
-    if (false && valid_symbols[ERROR_SENTINEL]) {
-        while (lexer->lookahead != '\n' && !lexer->eof(lexer))
-            skip(lexer);
-    }
+#ifdef DEBUG
+    char lookahead_at_start = lexer->lookahead;
+#endif
 
     bool nbsp = true;
     bool white_space = false;
@@ -39,6 +48,9 @@ bool tree_sitter_xpo_external_scanner_scan(void *payload, TSLexer *lexer, const 
             current_indent = 0;
             nbsp = false;
             skip(lexer);
+#ifdef DEBUG
+            scanner->row++;
+#endif
         } else if (lexer->lookahead == ' ') {
             white_space = true;
             current_indent++;
@@ -62,48 +74,101 @@ bool tree_sitter_xpo_external_scanner_scan(void *payload, TSLexer *lexer, const 
 
     lexer->mark_end(lexer);
 
-    uint32_t last_indent = scanner->indents.size > 0 ? *array_back(&scanner->indents) : 0;
-    bool next_token_is_a_tag = lexer->lookahead != '*' && lexer->lookahead != ';';
+    bool next_token_is_a_comment = lexer->lookahead == ';';
+
+#ifdef DEBUG
+    fprintf(stderr, "% 3d/% 3d (% 3d): ", scanner->row, current_indent, scanner->last_indent);
+
+    fprintf(stderr,
+            "%c%c%c%c%c%c|%c%c%c ",
+            valid_symbols[INDENT] ? 'I' : ' ',
+            valid_symbols[DEDENT] ? 'D' : ' ',
+            valid_symbols[NBSP] ? 'B' : ' ',
+            valid_symbols[NEWLINE] ? 'N' : ' ',
+            valid_symbols[WHITESPACE] ? 'W' : ' ',
+            valid_symbols[ERROR_SENTINEL] ? 'E' : ' ',
+            white_space ? '_' : ' ',
+            found_end_of_line ? '$' : ' ',
+            next_token_is_a_comment ? ';' :' ');
+
+    if (isprint(lookahead_at_start)) {
+        fprintf(stderr, "'%c'..", lookahead_at_start);
+    } else {
+        fprintf(stderr, " %02x..", lookahead_at_start);
+    }
+
+    if (isprint(lexer->lookahead)) {
+        fprintf(stderr, "'%c' ", lexer->lookahead);
+    } else {
+        fprintf(stderr, " %02x ", lexer->lookahead);
+    }
+#endif
+    if (valid_symbols[DEDENT] && current_indent < scanner->last_indent && !next_token_is_a_comment) {
+        scanner->last_indent -= INDENT_SIZE;
+        lexer->result_symbol = DEDENT;
+#ifdef DEBUG
+        fprintf(stderr, ": DEDENT %d->%d\n", scanner->last_indent, current_indent);
+#endif
+        return true;
+    }
+
+    if (valid_symbols[NEWLINE] && current_indent == scanner->last_indent && !next_token_is_a_comment) {
+#ifdef DEBUG
+        fprintf(stderr, ": NEWLINE\n");
+#endif
+        lexer->result_symbol = NEWLINE;
+        return true;
+    }
 
     if (found_end_of_line) {
-        if (valid_symbols[INDENT] && current_indent > last_indent) {
-            array_push(&scanner->indents, current_indent);
+        if (valid_symbols[INDENT] && current_indent > scanner->last_indent + 1) {
+            scanner->last_indent += INDENT_SIZE;
             lexer->result_symbol = INDENT;
-            return true;
-        }
-
-        if (valid_symbols[DEDENT] && scanner->indents.size > 0 && current_indent < last_indent) {
-            (void) array_pop(&scanner->indents);
-            lexer->result_symbol = DEDENT;
+#ifdef DEBUG
+            fprintf(stderr, ": INDENT %d->%d\n", scanner->last_indent, current_indent);
+#endif
             return true;
         }
 
         if (valid_symbols[NEWLINE]) {
-            if (current_indent > last_indent) {
-                array_push(&scanner->indents, current_indent);
+#ifdef DEBUG
+            fprintf(stderr, ": NEWLINE");
+#endif
+            if (!next_token_is_a_comment && current_indent > scanner->last_indent) {
+#ifdef DEBUG
+                fprintf(stderr, " %d->%d", scanner->last_indent, current_indent & ~0x1);
+#endif
+                scanner->last_indent = current_indent & ~0x1;
             }
+#ifdef DEBUG
+            fprintf(stderr, "\n");
+#endif
             lexer->result_symbol = NEWLINE;
             return true;
         }
-
-    } else if (next_token_is_a_tag && valid_symbols[DEDENT] && current_indent < last_indent &&
-               scanner->indents.size > 0) {
-        (void) array_pop(&scanner->indents);
-        lexer->result_symbol = DEDENT;
-        return true;
     }
 
     if (white_space) {
         if (nbsp && valid_symbols[NBSP]) {
             lexer->result_symbol = NBSP;
+#ifdef DEBUG
+            fprintf(stderr, ": NBSP\n");
+#endif
             return true;
         }
 
         if (valid_symbols[WHITESPACE]) {
             lexer->result_symbol = WHITESPACE;
+#ifdef DEBUG
+            fprintf(stderr, ": WHITESPACE\n");
+#endif
             return true;
         }
     }
+
+#ifdef DEBUG
+    fprintf(stderr, ": ---\n");
+#endif
 
     return false;
 }
@@ -113,12 +178,16 @@ unsigned tree_sitter_xpo_external_scanner_serialize(void *payload, char *buffer)
 
     size_t size = 0;
 
-    uint32_t iter = 1;
-    for (; iter < scanner->indents.size && size < TREE_SITTER_SERIALIZATION_BUFFER_SIZE; ++iter) {
-        uint16_t indent_value = *array_get(&scanner->indents, iter);
-        buffer[size++] = (char)(indent_value & 0xFF);
-        buffer[size++] = (char)((indent_value >> 8) & 0xFF);
-    }
+    buffer[size++] = (char)(scanner->last_indent & 0xFF);
+    buffer[size++] = (char)((scanner->last_indent >> 8) & 0xFF);
+    buffer[size++] = (char)((scanner->last_indent >> 16) & 0xFF);
+    buffer[size++] = (char)((scanner->last_indent >> 24) & 0xFF);
+#ifdef DEBUG
+    buffer[size++] = (char)(scanner->row & 0xFF);
+    buffer[size++] = (char)((scanner->row >> 8) & 0xFF);
+    buffer[size++] = (char)((scanner->row >> 16) & 0xFF);
+    buffer[size++] = (char)((scanner->row >> 24) & 0xFF);
+#endif
 
     return size;
 }
@@ -128,26 +197,30 @@ void tree_sitter_xpo_external_scanner_deserialize(void *payload, const char *buf
 
     if (length > 0) {
         size_t size = 0;
-
-        array_delete(&scanner->indents);
-        array_push(&scanner->indents, 0);
-
-        for (; size + 1 < length; size += 2) {
-            uint16_t indent_value = (unsigned char)buffer[size] | ((unsigned char)buffer[size + 1] << 8);
-            array_push(&scanner->indents, indent_value);
-        }
+        scanner->last_indent = (unsigned char)buffer[size++];
+        scanner->last_indent = scanner->last_indent | ((unsigned char)buffer[size++] << 8);
+        scanner->last_indent = scanner->last_indent | ((unsigned char)buffer[size++] << 16);
+        scanner->last_indent = scanner->last_indent | ((unsigned char)buffer[size++] << 24);
+#ifdef DEBUG
+        scanner->row = (unsigned char)buffer[size++];
+        scanner->row = scanner->row | ((unsigned char)buffer[size++] << 8);
+        scanner->row = scanner->row | ((unsigned char)buffer[size++] << 16);
+        scanner->row = scanner->row | ((unsigned char)buffer[size++] << 24);
+#endif
     }
 }
 
 void *tree_sitter_xpo_external_scanner_create() {
     Scanner *scanner = calloc(1, sizeof(Scanner));
-    array_init(&scanner->indents);
+    scanner->last_indent = 0;
+#ifdef DEBUG
+    scanner->row = 0;
+#endif
     tree_sitter_xpo_external_scanner_deserialize(scanner, NULL, 0);
     return scanner;
 }
 
 void tree_sitter_xpo_external_scanner_destroy(void *payload) {
     Scanner *scanner = (Scanner *)payload;
-    array_delete(&scanner->indents);
     free(scanner);
 }
